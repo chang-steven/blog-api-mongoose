7c5a725f0e97487f0d85ccf887bc886aeca461ed
Chang-Steven <steven.chang.dev@gmail.com>
2017-10-18 20:40:26 -0700

    Added get by ID and post method endpoints

--- models.js
+++ models.js
@@ -3,9 +3,25 @@ const mongoose = require('mongoose');
 const blogPostSchema =  mongoose.Schema({
       title: {type: String, required: true},
       content: {type: String, required: true},
-      author: {type: String, required: true},
-      created: {type: Date}
-  })
+      author: {
+        firstName: {type: String, required: true},
+        lastName: {type: String, required: true}
+      }
+  });
+
+blogPostSchema.virtual('authorString').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`
+});
+
+blogPostSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorString
+  }
+}
+
 
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
--- server.js
+++ server.js
@@ -13,11 +13,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan('common'))
 
-app.get('/blog-post', (req, res) => {
+app.get('/posts', (req, res) => {
   BlogPost
   .find()
   .then(result => {
-    res.json(result);
+    res.json({posts: result.map(
+      result => result.apiRepr())
+    });
   })
   .catch(
       err => {
@@ -25,6 +27,43 @@ app.get('/blog-post', (req, res) => {
         res.status(500).json({message: 'Internal server error'});
     });
 });
+
+app.get('/posts/:id', (req,res) => {
+  BlogPost
+  .findById(req.params.id)
+  .then(result => res.json(result.apiRepr()))
+  .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+app.post('/posts', jsonParser, (req,res) => {
+  const requiredFields = ['author', 'content', 'title'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    if (!(requiredFields[i] in req.body)) {
+      const message = `Sorry, missing ${requiredFields[i]} in post`
+      console.error(message);
+      return res.status(400).send(message);
+    };
+  };
+  BlogPost.
+  create( {
+    author: {
+      firstName: req.body.author.firstName,
+      lastName: req.body.author.lastName
+    },
+    title: req.body.title,
+    content: req.body.content
+  })
+  .then(result => res.status(201).json(result.apiRepr()))
+  .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
 
 let server;
 
